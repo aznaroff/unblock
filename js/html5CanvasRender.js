e262361f724d55fcc72512e5bc2a3fe9e78750d7
aznaroff <aznaroff@gmail.com>
2012-10-11 00:20:58 +0200

    add firefox support

--- js/html5CanvasRender.js
+++ js/html5CanvasRender.js
@@ -18,11 +18,11 @@ function Render(visual, id) {
 			var element = elements.controls[x];
 			if(typeof element == "object") {
 				ctx.fillStyle = '#000';
-				ctx.font = element.fontSize+'px sans-serif';
-				ctx.textBaseline = 'top';
+				ctx.font = element.fontSize+'px monospace';
+				ctx.textBaseline = 'baseline';
 
 				ctx.strokeRect (element.left, element.top, element.right-element.left, element.bottom-element.top);
-				ctx.fillText(element.value, element.left+Math.floor(((element.right - element.left)-ctx.measureText(element.value).width)/2), element.top);
+				ctx.fillText(element.value, element.left+Math.floor(((element.right - element.left)-ctx.measureText(element.value).width)/2), element.bottom - element.fontSize/7);
 			}
 		}
 		//grille
@@ -53,42 +53,46 @@ function Render(visual, id) {
 	visual.addEventListener("win", function() {
 		alert(visual.getWinMessage());
 	});
-	document.addEventListener("mousewheel", function(event) {
-		visual.wheeling(event.offsetX, event.offsetY, -1*(event.wheelDelta/120));
+	document.addEventListener("DOMMouseScroll", function(event) {//firefox
+		visual.wheeling(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop, event.detail/Math.abs(event.detail));
 		event.preventDefault();
-	});
+	}, false);
+	document.addEventListener("mousewheel", function(event) {//webkit
+		visual.wheeling(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop, -1*(event.wheelDelta/120));
+		event.preventDefault();
+	}, false);
 	document.addEventListener("mousemove", function(event) {
-		visual.pointerMove(event.offsetX, event.offsetY);
+		visual.pointerMove(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
 		event.preventDefault();
-	});
+	}, false);
 	document.addEventListener("mousedown", function(event) {
-		visual.pointerDown(event.offsetX, event.offsetY);
+		visual.pointerDown(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
 		event.preventDefault();
-	});
+	}, false);
 	document.addEventListener("mouseup", function(event) {
-		visual.pointerUp(event.offsetX, event.offsetY);
+		visual.pointerUp(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
 		event.preventDefault();
-	});
+	}, false);
 	document.addEventListener("touchmove", function(event) {
 		last.x = event.touches[0].pageX-canvas.offsetLeft;
 		last.y = event.touches[0].pageY-canvas.offsetTop;
 		visual.pointerMove(event.touches[0].pageX-canvas.offsetLeft, event.touches[0].pageY-canvas.offsetTop);
 		event.preventDefault();
-	});
+	}, false);
 	document.addEventListener("touchstart", function(event) {
 		last.x = event.touches[0].pageX-canvas.offsetLeft;
 		last.y = event.touches[0].pageY-canvas.offsetTop;
 		visual.pointerDown(event.touches[0].pageX-canvas.offsetLeft, event.touches[0].pageY-canvas.offsetTop);
 		event.preventDefault();
-	});
+	}, false);
 	document.addEventListener("dblclick", function(event) {
 		event.preventDefault();
-	});
+	}, false);
 	document.addEventListener("touchend", function(event) {
 		if(event.touches.length > 0) {
 			visual.pointerUp(event.touches[0].pageX-canvas.offsetLeft, event.touches[0].pageY-canvas.offsetTop);
 		} else
 			visual.pointerUp(last.x, last.y);
 		event.preventDefault();
-	});
+	}, false);
 }
